0c31e16d5702aef3047507cae09f3b92071d12f4
TheBrightestHoneyRay <balnurquarter18@gmail.com>
2023-04-11 20:20:23 +0600

    sort top ten

--- lab9/hh_back/api/urls.py
+++ lab9/hh_back/api/urls.py
@@ -5,7 +5,8 @@ from api import views
 urlpatterns = [
     path('companies/', views.companies),
     path('companies/<int:company_id>/', views.company),
-    path('companies/<int:company_id>/vacancy', views.vacancy_by_company),
+    path('companies/<int:company_id>/vacancies', views.vacancy_by_company),
     path('vacancies/', views.vacancies),
-    path('vacancies/<int:vacancy_id>', views.vacancy)
+    path('vacancies/<int:vacancy_id>', views.vacancy),
+    path('vacancies/top_ten/', views.top_ten)
 ]
--- lab9/hh_back/api/views.py
+++ lab9/hh_back/api/views.py
@@ -13,15 +13,26 @@ def companies(request):
 
 def company(request, company_id):
     comp_list = Company.objects.all()
-    comp = comp_list.get(id=company_id).to_json()
+
+    try:
+        comp = comp_list.get(id=company_id).to_json()
+    except Company.DoesNotExist as e:
+        return JsonResponse({'error': str(e)}, status=400)
+
     return JsonResponse(comp, safe=False)
 
 
 def vacancy_by_company(request, company_id):
     comp_list = Company.objects.all()
     vacancy_list = Vacancy.objects.all()
+
+    try:
+        comp = comp_list.get(id=company_id)
+    except Company.DoesNotExist as e:
+        return JsonResponse({'error': str(e)}, status=400)
+
     vacan_json = []
-    comp = comp_list.get(id=company_id)
+
     for v in vacancy_list:
         if v.company == comp:
             vacan_json.append(v.to_json())
@@ -31,12 +42,29 @@ def vacancy_by_company(request, company_id):
 def vacancies(request):
     vacan_list = Vacancy.objects.all()
     vacan_json = []
+
     for v in vacan_list:
         vacan_json.append(v.to_json())
+
     return JsonResponse(vacan_json, safe=False)
 
 
 def vacancy(request, vacancy_id):
     vacan_list = Vacancy.objects.all()
-    vacan = vacan_list.get(id=vacancy_id).to_json()
+
+    try:
+        vacan = vacan_list.get(id=vacancy_id).to_json()
+    except Vacancy.DoesNotExist as e:
+        return JsonResponse({'error': str(e)}, status=400)
+
     return JsonResponse(vacan, safe=False)
+
+
+def top_ten(request):
+    tops = Vacancy.objects.all().order_by('-salary')[:10]
+    tops_json = []
+
+    for t in tops:
+        tops_json.append(t.to_json())
+
+    return JsonResponse(tops_json, safe=False)
